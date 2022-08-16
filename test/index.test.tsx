5c108a6386e21212e21d476911d23b881bae89a9
Chris Cowan <agentme49@gmail.com>
2022-08-15 20:08:08 -0700

    add failing tests for update issues

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,3 +1,4 @@
+import { createRef, forwardRef, Ref, useImperativeHandle, useState } from 'react';
 import TestRenderer, { act } from 'react-test-renderer';
 
 import { Wizard } from "../src";
@@ -21,5 +22,75 @@ test('next works', () => {
   expect(testRenderer.root.findByType('div').props.id).toBe('start-div');
   act(() => testRenderer.root.findByType('div').props.onClick());
   expect(testRenderer.root.findByType('div').props.id).toBe('next-div');
+  testRenderer.unmount();
+});
+
+test('can update steps', () => {
+  const testRenderer = TestRenderer.create(
+    <Wizard
+      initial='start'
+      start={() => (
+        <span>span text 1</span>
+      )}
+    />
+  );
+
+  expect(testRenderer.root.findByType('span').props.children).toBe('span text 1');
+
+  testRenderer.update(
+    <Wizard
+      initial='start'
+      start={() => (
+        <span>span text 2</span>
+      )}
+    />
+  );
+
+  expect(testRenderer.root.findByType('span').props.children).toBe('span text 2');
+  testRenderer.unmount();
+});
+
+test('can update steps without losing state', () => {
+  interface CWSProps {
+    defaultValue: string;
+  }
+
+  interface CWSRef {
+    setValue(value: string): void;
+  }
+
+  const ComponentWithState = forwardRef((props: CWSProps, ref: Ref<CWSRef>) => {
+    const [value, setValue] = useState(props.defaultValue);
+    useImperativeHandle(ref, () => ({
+      setValue
+    }), [setValue]);
+    return <div id="cws">{value}</div>;
+  });
+
+  const cwsRef = createRef<CWSRef>();
+
+  const testRenderer = TestRenderer.create(
+    <Wizard
+      initial='start'
+      start={() => (
+        <ComponentWithState ref={cwsRef} defaultValue="initial value" />
+      )}
+    />
+  );
+
+  expect(testRenderer.root.findByType('div').props.children).toBe('initial value');
+  act(() => cwsRef.current!.setValue('new value'));
+  expect(testRenderer.root.findByType('div').props.children).toBe('new value');
+
+  testRenderer.update(
+    <Wizard
+      initial='start'
+      start={() => (
+        <ComponentWithState ref={cwsRef} defaultValue="initial value" />
+      )}
+    />
+  );
+
+  expect(testRenderer.root.findByType('div').props.children).toBe('new value');
   testRenderer.unmount();
 });
