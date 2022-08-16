5646d8d4c545fdaad565c184aa7d3740311e129f
Chris Cowan <agentme49@gmail.com>
2022-08-15 19:42:03 -0700

    add basic test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,25 @@
-describe('Thing', () => {
-  it('just works', () => {
-    expect(true).toBe(true);
-  });
+import TestRenderer, { act } from 'react-test-renderer';
+
+import { Wizard } from "../src";
+
+test('next works', () => {
+  const testRenderer = TestRenderer.create(
+    <Wizard
+      initial='start'
+      start={({ next, prev, goto }) => (
+        <div id="start-div" onClick={next}>start</div>
+      )}
+      theNextStep={({ goto }) => (
+        <div id="next-div">the next step</div>
+      )}
+      finish={() => (
+        <div>finish</div>
+      )}
+    />
+  );
+
+  expect(testRenderer.root.findByType('div').props.id).toBe('start-div');
+  act(() => testRenderer.root.findByType('div').props.onClick());
+  expect(testRenderer.root.findByType('div').props.id).toBe('next-div');
+  testRenderer.unmount();
 });
